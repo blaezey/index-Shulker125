285a6c13bf48915f99eb1210d4d4f60a6f8a948c
Zayne Hussain <78383186+Shulker125@users.noreply.github.com>
2021-02-22 19:58:51 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,1 @@
-$(document).ready(function(){
-        $('.list .link a').click(function(){
-        $('.link a').removeClass("active");
-        $(this).addClass("active");
-       });
-       });
+
